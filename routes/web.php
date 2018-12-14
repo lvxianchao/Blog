15ca09ef013922af13a790d56c906a4d4615e228
lxc <coderlxc@gmail.com>
2018-12-14 22:59:59 +0800

    将模型转移到目录中

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@
 Route::get('/admin/login', 'Admin\LoginController@showLoginForm')->name('login');
 Route::post('/admin/login', 'Admin\LoginController@login');
 Route::post('/admin/logout', 'Admin\LoginController@logout')->name('logout');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
