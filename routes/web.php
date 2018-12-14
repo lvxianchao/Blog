e2f94b611fc392e0344eb0274126249f38649d4e
lxc <coderlxc@gmail.com>
2018-12-14 22:53:27 +0800

    添加了 Lravel IDE Helper

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+// 登录退出
+Route::get('/admin/login', 'Admin\LoginController@showLoginForm')->name('login');
+Route::post('/admin/login', 'Admin\LoginController@login');
+Route::post('/admin/logout', 'Admin\LoginController@logout')->name('logout');
