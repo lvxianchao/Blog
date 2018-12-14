935221b1f4574fb5a946d3f659e93757d68024b3
lxc <coderlxc@gmail.com>
2018-12-14 23:43:00 +0800

    登录页面调整

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ Route::get('/admin/login', 'Admin\LoginController@showLoginForm')->name('login')
 Route::post('/admin/login', 'Admin\LoginController@login');
 Route::post('/admin/logout', 'Admin\LoginController@logout')->name('logout');
 
-Route::middleware('auth')->prefix('admin.')->name('admin')->namespace('Admin')->group(function () {
+Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
     // 后台主页
-    Route::get('dashboard', 'Admin\DashboardController@index')->name('index');
+    Route::get('dashboard', 'Admin\DashboardController@index')->name('dashboard');
 });
