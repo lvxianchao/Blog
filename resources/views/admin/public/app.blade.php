087086975cf5d75088b966a658d4ce4acda7fa7c
lxc <coderlxc@gmail.com>
2018-12-17 00:26:31 +0800

    使用七牛静态资源的 Bootstrap 样式

--- resources/views/admin/public/app.blade.php
+++ resources/views/admin/public/app.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="stylesheet" href="{{ asset('vendor/layui/css/layui.css') }}">
+        <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <title>{{ config('app.name', 'Laravel') }} | @yield('title')</title>
         @yield('styles')
