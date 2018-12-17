01098a48a54667e54eb633bc65ece005d3abb2bf
lxc <coderlxc@gmail.com>
2018-12-17 22:41:22 +0800

    菜单图标

--- resources/views/admin/public/_menu.blade.php
+++ resources/views/admin/public/_menu.blade.php
@@ -4,21 +4,21 @@
             {{--Dashboard--}}
             <li class="layui-nav-item {{ active_class(if_route('admin.dashboard'), 'layui-this') }}">
                 <a href="{{ route('admin.dashboard') }}">
-                    <i class="fa fa-columns"></i>
+                    <i class="fa fa-dashboard"></i>
                     <span>Dashboard</span>
                 </a>
             </li>
             {{--标签管理--}}
             <li class="layui-nav-item {{ active_class(if_route_pattern('admin.tags.*'), 'layui-this') }}">
                 <a href="{{ route('admin.tags.index') }}">
-                    <i class="fa fa-columns"></i>
+                    <i class="fa fa-tags"></i>
                     <span>标签管理</span>
                 </a>
             </li>
             {{--文章管理--}}
             <li class="layui-nav-item {{ active_class(if_route_pattern('admin.posts.*'), 'layui-this') }}">
                 <a href="{{ route('admin.posts.index') }}">
-                    <i class="fa fa-columns"></i>
+                    <i class="fa fa-edit"></i>
                     <span>文章管理</span>
                 </a>
             </li>
